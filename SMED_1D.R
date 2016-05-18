3a87b7403dcb76464ab75b45cac761a94c9d5f81
Collin Erickson <collinberickson@gmail.com>
2016-05-18 18:29:55 -0500

    Deleted old code

--- SMED_1D.R
+++ SMED_1D.R
@@ -1,39 +1,4 @@
-if (F){
-  # Scratch work, look below for function
-  f <- Vectorize(function(xx){dbeta(xx,4,2)})
-  curve(f)
-  qq <- function(xx){f(xx)^-.5}
-  f_min <- Vectorize(function(xnew,xall,k=4) {
-    #min(
-      qq(xnew)^k*sum(sapply(xall,function(xx){(qq(xx)/abs(xx-xnew))^k}))
-        #,1e100) # Thought Inf was a problem, still might be
-  }, vectorize.args='xnew')
-  curve(f_min(x,c(.75001)))
-  
-  # Initialize with mode
-  curve(f)
-  #X <- c(.75)
-  xavail <- runif(1000)
-  xmodeind <- which.max(f(xavail))
-  X <- xavail[xmodeind]
-  text(X,f(X),1,col='red')
-  xavail <- xavail[-xmodeind]
-  for(i in 2:30) {
-    #f_opt <- optimize(function(xx){f_min(xx,X)},interval = c(0,1),temp='SANN')
-    #xnew <- f_opt$min
-    f_opt <- which.min(f_min(xavail,X))
-    xnew <- xavail[f_opt]
-    X <- c(X,xnew)
-    xavail <- xavail[-f_opt]
-    text(xnew,f(xnew),i,col='red')
-    points(xnew,0,col=2)
-  }
-  #curve(f_min(x,X),ylim=c(1e6,1e11))
-  hist(X,freq=F)
-  curve(f,add=T,col='red')
-}
 
-# Here's the function
 SMED_1D <- function(f,n=10,navail=1000,kk=4,xmin=0,xmax=1,include.histogram=F) {
   # Function that implements SMED in 1D. Estimates probability distribution
   #  f: density function
--- SMED_2D.R
+++ SMED_2D.R
@@ -1,52 +1,3 @@
-if (F) {
-  #f <- function(xx){apply(xx,1,function(yy){exp(-.005*yy[1]-.5*(yy[2]+.03*yy[1]^2-3)^2)})}
-  f <- function(xx){
-    if(is.matrix(xx)) {
-      return(apply(xx,1,function(yy){exp(-.005*(yy[1]*40-20)^2-.5*(yy[2]*15-10+.03*(yy[1]*40-20)^2-3)^2)}))
-    } else {
-      yy <- xx
-      exp(-.005*(yy[1]*40-20)^2-.5*(yy[2]*15-10+.03*(yy[1]*40-20)^2-3)^2)
-    }
-  }
-  
-  # Get contour plot
-  nc <- 100 # number contour each dimension
-  fx <- fy <- seq(0,1,length.out = nc)
-  fz <- matrix(0,nc,nc)
-  for (xi in 1:nc) for(yi in 1:nc) fz[xi,yi] <- f(matrix(c(fx[xi],fy[yi]),1,2))
-  contour(fx,fy,fz,nlevels = 5)
-  my.filled.contour(fx,fy,fz,nlevels = 5)
-  
-  p <- 2 # dimension
-  k <- 4*p # MED distance thing
-  
-  qq <- function(xx){f(xx)^-(1/(2*p))}
-  f_min <- function(xnew,xall,kk) {
-    #print(c())
-    #print(c(qq(xnew)^kk,-12,(apply(xall,1,function(xx){(qq(xx)/(sqrt(sum((xx-xnew)^2))))^kk}))))
-    #qq(xnew)^k*sum(sapply(xall,function(xx){(qq(xx)/abs(xx-xnew))^k}))
-    qq(xnew)^kk*sum(apply(xall,1,function(xx){(qq(xx)/(sqrt(sum((xx-xnew)^2))))^kk}))
-  }
-  
-  
-  # Initialize with mode
-  gsa.out <- GenSA::GenSA(par=NULL,fn=function(xx)-f(xx),lower=c(0,0),upper=c(1,1),control = list(trace.mat=F))
-  X <- matrix(gsa.out$par,1,2)
-  text(X[1],X[2],labels=1,col=1,pch=1)
-  for(i in 2:30) {
-    # GenSA is slow, maybe change parameters to limit time
-    #gsa.out <- GenSA::GenSA(par=NULL,fn=function(xx)log(f_min(xx,X,kk=k)),lower=c(0,0),upper=c(1,1),control = list(maxit=10,trace.mat=F,max.time=1,max.call=10))
-    
-    # Or use default
-    gsa.out <- GenSA::GenSA(par=NULL,fn=function(xx)log(f_min(xx,X,kk=k)),lower=c(0,0),upper=c(1,1),control = list(trace.mat=F,max.time=1))
-  
-    # Add new point
-    xnew <- gsa.out$par
-    X <- rbind(X,xnew)
-    text(x=xnew[1],y=xnew[2],labels=i,col=1)
-  }
-}
-
 SMED_2D <- function(f,n=10,nc=100,max.time=NULL) {
   # Function for SMED in 2D
   # Input:
@@ -94,4 +45,5 @@ if (F) {
   setwd("C:/Users/cbe117/School/DOE/SMED/SMED-Code")
   source('TestFunctions.R')
   SMED_2D(banana,n=10,max.time=.2)
+  SMED_2D(banana,n=40,max.time=.2)
 }
