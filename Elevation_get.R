268a7a12cfe10336dc6c7a70de1fa04ac20c4ed1
Collin Erickson <collinberickson@gmail.com>
2016-05-23 14:50:01 -0500

    Changed myfilledcontour to contourfilled, added get contour from data

--- Elevation_get.R
+++ Elevation_get.R
@@ -5,6 +5,7 @@ if (F) {
   
   
   require(XML)
+  require(RCurl)
   data_parse <- xmlParse("http://forecast.weather.gov/MapClick.php?lat=29.803&lon=-82.411&FcstType=digitalDWML")
   data_parse <- xmlParse("https://maps.googleapis.com/maps/api/elevation/xml?locations=39.7391536,-104.9847034|36.455556,-116.866667&key=AIzaSyDpHsTEEAWJqE1MbWnMjwJI_Yo54CUviw0")
   xml_data <- xmlToList(data)
@@ -13,6 +14,11 @@ if (F) {
   elev_url <- "https://maps.googleapis.com/maps/api/elevation/xml?locations=39.7391536,-104.9847034|36.455556,-116.866667&key=AIzaSyDpHsTEEAWJqE1MbWnMjwJI_Yo54CUviw0"
   elev_html <- getURL(elev_url)
   elev_xml <- xmlParse(elev_html)
+  elevs <- unname(xmlToDataFrame(nodes = getNodeSet((elev_xml),'//result/elevation')))
+  resols <- unname(xmlToDataFrame(nodes = getNodeSet((elev_xml),'//result/resolution')))
+  lats <- unname(xmlToDataFrame(nodes = getNodeSet((elev_xml),'//result/location/lat')))
+  longs <- unname(xmlToDataFrame(nodes = getNodeSet((elev_xml),'//result/location/lng')))
+  data.frame(lat=lats,lng=longs,elev=elevs,res=resols)
   #elev_xml[[1]]
   
   elev_xml <- xmlTreeParse(elev_html)
@@ -38,6 +44,7 @@ get.elevation <- function(X) {
   #  2,500 free requests per day
   #  512 locations per request
   #  10 requests per second
+  #  Lower resolution for larger requests
   # X: n by 2 matrix
   X <- matrix(round(X,6),ncol=2)
   #X <- X[,c(2,1)]
@@ -51,34 +58,55 @@ get.elevation <- function(X) {
   elev_url <- paste0(elev_url,"&key=AIzaSyDpHsTEEAWJqE1MbWnMjwJI_Yo54CUviw0")
   
   elev_html <- getURL(elev_url)
-  elev_xml <- xmlTreeParse(elev_html)
+  #elev_xml <- xmlTreeParse(elev_html)
   
-  for(inum in 1:(length(X)/2)) {
+  #for(inum in 1:(length(X)/2)) {
     #browser()
     #print(elev_xml)
     #browser()
-    lat <-  as.numeric(as.character(elev_xml[[1]][[1]][[1+inum]][[1]][[1]][[1]])[6])
-    long <- as.numeric(as.character(elev_xml[[1]][[1]][[1+inum]][[1]][[2]][[1]])[6])
-    elev <- as.numeric(as.character(elev_xml[[1]][[1]][[1+inum]][[2]][[1]])[6])
-    resol <- as.numeric(as.character(elev_xml[[1]][[1]][[1+inum]][[3]][[1]])[6])
-    newelevdf <- data.frame(lat,long,elev,resol)
-    if(inum==1) elev_df <- newelevdf
-    else elev_df <- rbind(elev_df,newelevdf)
+  #  lat <-  as.numeric(as.character(elev_xml[[1]][[1]][[1+inum]][[1]][[1]][[1]])[6])
+  #  long <- as.numeric(as.character(elev_xml[[1]][[1]][[1+inum]][[1]][[2]][[1]])[6])
+  #  elev <- as.numeric(as.character(elev_xml[[1]][[1]][[1+inum]][[2]][[1]])[6])
+  #  resol <- as.numeric(as.character(elev_xml[[1]][[1]][[1+inum]][[3]][[1]])[6])
+  #  newelevdf <- data.frame(lat,long,elev,resol)
+  #  if(inum==1) elev_df <- newelevdf
+  #  else elev_df <- rbind(elev_df,newelevdf)
     #newelevdf <- data.frame(lat,long,elev,resol)
+  #}
+  #browser()
+  if (grepl(pattern = 'illegal request',x = elev_html)) {
+    stop('Illegal request (probably too long a request)')
   }
-  elev_df$elev
+  elev_parse <- xmlParse(elev_html)
+  status <- xmlToDataFrame(nodes = getNodeSet((elev_parse),'//status'),stringsAsFactors = F)
+  if (xmlToDataFrame(nodes = getNodeSet((elev_parse),'//status'))$text[1]=='OK') {
+    elevs <- unname(xmlToDataFrame(nodes = getNodeSet((elev_parse),'//result/elevation'),colClasses = 'numeric'))
+    resols <- unname(xmlToDataFrame(nodes = getNodeSet((elev_parse),'//result/resolution'),colClasses = 'numeric'))
+    lats <- unname(xmlToDataFrame(nodes = getNodeSet((elev_parse),'//result/location/lat'),colClasses = 'numeric'))
+    longs <- unname(xmlToDataFrame(nodes = getNodeSet((elev_parse),'//result/location/lng'),colClasses = 'numeric'))
+    elev_df <- data.frame(lat=lats,lng=longs,elev=elevs,res=resols)
+  } else {
+    #browser()
+    stop('API request was not successful (status was not OK)')
+  }
+  return(elev_df)
 }
-rescale1D <- function(x,xlim) {
-  (x-min(x))/(max(x)-min(x)) * (xlim[2] - xlim[1]) + xlim[1]
+rescale1D <- function(x,xlim,oldxlim=NULL) {
+  if (is.null(oldxlim)) {oldxlim <- c(min(x),max(x))}
+  (x-oldxlim[1])/(oldxlim[2]-oldxlim[1]) * (xlim[2] - xlim[1]) + xlim[1]
 }
-rescale2D <- function(x,xlim,ylim) {
+rescale2D <- function(x,xlim,ylim,oldxlim=NULL,oldylim=NULL) {
   if (dim(x)[2] != 2) {stop('x must have two columns')}
-  x[,1] <- rescale1D(x[,1],xlim)
-  x[,2] <- rescale1D(x[,2],ylim)
+  x[,1] <- rescale1D(x[,1],xlim,oldxlim)
+  x[,2] <- rescale1D(x[,2],ylim,oldylim)
   return(x)
 }
 
 if (F) {
+  setwd("C:/Users/cbe117/School/DOE/SMED/SMED-Code")
+  source('C:/Users/cbe117/School/DOE/SMED/SMED-Code/myfilledcontour.R')
+  require(RCurl)
+  require(XML)
   StLouisCntyMN <- c(47.882,-92.476)
   SWSanJose <- c(36.504,-123.106)
   NWCol <- c(40.999198, -109.054831)
@@ -87,8 +115,9 @@ if (F) {
   Colorado.ylim <- c(36.996650,40.999198)
   X.LHS <- lhs::maximinLHS(n=40,k=2)
   X.Colorado <- t(apply(X.LHS,1,function(xrow) return(c(xrow[1]*(Colorado.xlim[2]-Colorado.xlim[1])+Colorado.xlim[1],xrow[2]*(Colorado.ylim[2]-Colorado.ylim[1])+Colorado.ylim[1]))))
-  X.Colorado <- rescale2D(X.LHS,Colorado.xlim,Colorado.ylim)
+  X.Colorado2 <- rescale2D(X.LHS,Colorado.xlim,Colorado.ylim,0:1,0:1)
+  cbind(X.Colorado,X.Colorado2)
   get.elevation(c(40,-100))
   # Google elevation only takes 92 at a time, 93+ will return an error
-  my.filled.contour.func(get.elevation,n=9,xcontlim=Colorado.xlim,ycontlim=Colorado.ylim,batchmax = 90)
+  my.filled.contour.func(get.elevation,n=21,xcontlim=Colorado.xlim,ycontlim=Colorado.ylim,batchmax = 90,out.col.name = 'elev')
 }
