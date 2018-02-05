d3cd0b2fc4b0a0cabc89ae53cea4822ae8311b66
Collin Erickson <collinberickson@gmail.com>
2018-02-05 11:02:00 -0600

    Updating SMED_GP_2D

--- R/SMED_GP_2D.R
+++ R/SMED_GP_2D.R
@@ -1,28 +1,43 @@
-SMED_GP_2D <- function(f,n0=10,n=10,nc=100,GP.package='',opt.method='genoud',contour.fit=0,continue.option=F) {
-  # Function that implements SMED with GP in 2 dimensions. Using the GP means you 
-  #  only get the function value from points you have selected, selection is 
+#' SMED GP 2D
+#'
+#' @param f Function
+#' @param n0 Number of initial points
+#' @param n Number of points to select
+#' @param GP.package  GP package
+#' @param opt.method Optimization method. Current options are 'genoud', 'LHS'.
+#' @param contour.fit Number of iterations after which to plot model contour plot.
+#' @param continue.option If TRUE, you are given choice to continue sampling
+#'
+#' @return X, design matrix, and Y, function values at design points.
+#' @export
+#'
+#' @examples
+#' SMED_GP_2D(TestFunctions::banana,10,10, GP.package='laGP')
+SMED_GP_2D <- function(f,n0=10,n=10,GP.package='',opt.method='genoud',contour.fit=1,continue.option=F) {
+  # Function that implements SMED with GP in 2 dimensions. Using the GP means you
+  #  only get the function value from points you have selected, selection is
   #  based on the GP model predictions
   # Input:
   #  n0: # of pts to start with, selected with maximin LHS
   #  n: # of pts to select -> total pts is n0+n
   #  nc: grid size of contour plots are nc by nc
-  #  GP.package: which R package to fit GP with. Current options are 
+  #  GP.package: which R package to fit GP with. Current options are
   #    GPfit, laGP, and mlegp. Also exact will use exact function (for debugging)
   #  opt.method: optimization method. Current options are 'genoud', 'LHS'.
   #  contour.fit: # of iterations after which to plot model contour plot
   #  continue.option: If TRUE, you are given choice to continue sampling
-  # Output: 
+  # Output:
   #  X: design matrix
   #  Y: function values at design points
-  
+
   p <- 2 # dimension
   k <- 4 * p # MED distance power, k=4p is recommended by Roshan
-  
+
   # source('TestFunctions.R') # Not sure if I should source test functions here or outside
   # source('myfilledcontour.R') # Source myfilledcontour.R to get contour plots
-  require('contourfilled') # Contour functions should come from package
-  require('lhs') # Use lhs to get initial points
-  
+  # require('contourfilled') # Contour functions should come from package
+  # require('lhs') # Use lhs to get initial points
+
   # Set GP functions for each package
   #  Need functions for init, update, predict, and delete
   if(GP.package=='GPfit') {
@@ -34,7 +49,8 @@ SMED_GP_2D <- function(f,n0=10,n=10,nc=100,GP.package='',opt.method='genoud',con
   } else if(GP.package=='laGP') {
     require('laGP')
     init.GP.SMED <- function(X,Y) {laGP::newGPsep(X=X,Z=Y,d=2,g=1e-8)}
-    update.GP.SMED <- function(mod,X,Y) {laGP::updateGPsep(gpsepi=mod,X=X,Z=Y);return(mod)}
+    # update.GP.SMED <- function(mod,X,Y) {laGP::updateGPsep(gpsepi=mod,X=X,Z=Y);return(mod)} # Would need only new X and Y
+    update.GP.SMED <- function(mod,X,Y) {laGP::deleteGPsep(mod);laGP::newGPsep(X=X,Z=Y,d=2,g=1e-8)}
     predict.GP.SMED <- function(mod,xx){max(1e-16,laGP::predGPsep(mod,matrix(xx,1,2))$mean)}
       # Changed this predict, really bad now
     delete.GP.SMED <- laGP::deleteGPsep
@@ -48,18 +64,18 @@ SMED_GP_2D <- function(f,n0=10,n=10,nc=100,GP.package='',opt.method='genoud',con
     predict.GP.SMED <- function(mod,xx) {f(xx)}
     init.GP.SMED <- function(X,Y) {}
     update.GP.SMED <- function(mod,X,Y) {}
-    delete.GP.SMED <- function(mod){}    
+    delete.GP.SMED <- function(mod){}
   } else {
     stop('No GP.package specified Error # 579238572')
   }
-  
-  
+
+
   # Charge function qq, no longer is actual function, now is based on predictions
   qq <- function(xx,mod){
     yy <- (predict.GP.SMED(mod=mod,xx=xx))^-(1/(2*p));
     if(!is.nan(yy)){
       return(yy)
-    } else { 
+    } else {
       browser();stop(paste('NaN qq value Error 357923',predict.GP.SMED(mod=mod,xx=xx)))
     }
   }
@@ -71,12 +87,13 @@ SMED_GP_2D <- function(f,n0=10,n=10,nc=100,GP.package='',opt.method='genoud',con
   log.f_min <- function(xnew,xall,kk,mod,qq.scale=1) {
     kk * log(qq(xnew,mod=mod)*qq.scale) + log(sum(apply(xall,1,function(xx){((qq(xx,mod=mod)*qq.scale)/(sqrt(sum((xx-xnew)^2))))^kk})))
   }
-  
-  
+
+
   # Get contour plot
   #my.filled.contour.func(fn=f,n=nc)
-  contourfilled.func(fn=f,n=nc)
-  
+  # contourfilled.func(fn=f,n=nc)
+  reset.plot <- ContourFunctions::cf(f, reset.par=F)
+
   # Initialize with LHS
   X <- lhs::maximinLHS(n=n0,k=2)
   Y <- apply(X,1,f)
@@ -84,17 +101,19 @@ SMED_GP_2D <- function(f,n0=10,n=10,nc=100,GP.package='',opt.method='genoud',con
   keep.Delta <- (Y > Delta)
   # Plot where points are
   text(X[,1],X[,2],col=ifelse(keep.Delta,'magenta','orange'))
-  
+
   # Initialize model
   mod <- init.GP.SMED(X,Y)
-  
+
   # Get rest of points, while loop lets you add points in when prompted
   i <- 1
   while(i <= n) {
     # Plot contour of fit if iteration is multiple of contour.fit
     if(contour.fit>0 & i%%contour.fit==0) {
       #my.filled.contour.func(function(xx)predict.GP.SMED(mod,xx))
-      contourfilled.func(function(xx)predict.GP.SMED(mod,xx))
+      # contourfilled.func(function(xx)predict.GP.SMED(mod,xx))
+      reset.plot()
+      reset.plot <- ContourFunctions::cf_func(function(xx)predict.GP.SMED(mod,xx), reset.par=F)
       text(x=X[,1],y=X[,2],col=ifelse(keep.Delta,'magenta','springgreen3'))
     }
     # Perform optimzation to select next point, use log scale
@@ -123,12 +142,12 @@ SMED_GP_2D <- function(f,n0=10,n=10,nc=100,GP.package='',opt.method='genoud',con
     X <- rbind(X,unname(xnew))
     Y <- c(Y,ynew)
     text(x=xnew[1],y=xnew[2],labels=n0+i,col=1) # Plot it on contour
-    
+
     # Delta is used to 'hide' points from the potential function that are not useful
     # Without this the points selected were terrible
     Delta <- .01 * (n0 / (n + i)) ^ (1 / p) * max(Y)
     keep.Delta <- (Y > Delta)
-    
+
     # Update model
     mod <- update.GP.SMED(mod,X,Y)
 
@@ -143,10 +162,13 @@ SMED_GP_2D <- function(f,n0=10,n=10,nc=100,GP.package='',opt.method='genoud',con
   }
   # Print final contour of estimated surface
   #my.filled.contour.func(function(xx)predict.GP.SMED(mod,xx))
-  contourfilled.func(function(xx)predict.GP.SMED(mod,xx))
+  # contourfilled.func(function(xx)predict.GP.SMED(mod,xx))
+  reset.plot()
+  reset.plot <- ContourFunctions::cf_func(function(xx)predict.GP.SMED(mod,xx), reset.par=F)
   text(x=X[,1],y=X[,2],col='magenta')
   # Delete model if needed (only laGP)
   delete.GP.SMED(mod)
+  reset.plot() # Reset plot parameters
   # Return design matrix and outputs
   return(cbind(X,Y))
 }
@@ -157,14 +179,14 @@ if (F) {
   SMED_GP_2D(f=banana,n0=50,n=5,contour.fit=1,GP.package='laGP',continue.option=T)
   SMED_GP_2D(f=function(x){exp(-(sum((x-.5)^2))/.01)},n0=10,n=5,contour.fit=1,GP.package='laGP',continue.option=T)
   SMED_GP_2D(f=banana,n0=10,n=5,contour.fit=1,GP.package='exact',continue.option=T)
-  
+
   # Comparing with seed, pick one point
   set.seed(0);SMED_GP_2D(f=banana,n0=30,n=1,contour.fit=1,GP.package='GPfit',continue.option=T,opt.method='LHS')
-  # 31 0.995832 0.9755436 9.061522e-41 18.48422 18.48422 22.16811 
+  # 31 0.995832 0.9755436 9.061522e-41 18.48422 18.48422 22.16811
   set.seed(0);SMED_GP_2D(f=banana,n0=30,n=1,contour.fit=1,GP.package='mlegp',continue.option=T,opt.method='LHS')
-  # 31 0.9891865 0.986211 7.488378e-40 18.44477 18.44477 32.09332 
+  # 31 0.9891865 0.986211 7.488378e-40 18.44477 18.44477 32.09332
   set.seed(0);SMED_GP_2D(f=banana,n0=30,n=1,contour.fit=1,GP.package='laGP',continue.option=T,opt.method='LHS')
   # 31 0.6320691 0.9985909 0.01649461 17.28631 17.28631 89.38975  Much worse fit though
   set.seed(0);SMED_GP_2D(f=banana,n0=30,n=1,contour.fit=1,GP.package='exact',continue.option=T,opt.method='LHS')
-  # 31 0.5693355 0.913252 0.6247042 20.49745 20.49745 118.301 
+  # 31 0.5693355 0.913252 0.6247042 20.49745 20.49745 118.301
 }
